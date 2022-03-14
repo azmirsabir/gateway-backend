27a8893fdf9e189786b2775aff49dbb7d4eb930c
Azmir.Sleman <Azmir.Sleman@korektel.com>
2022-03-14 09:38:50 +0300

    dockerize

--- routes/api.php
+++ routes/api.php
@@ -1,9 +1,7 @@
 <?php
 
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\AuthController;
 use App\Http\Controllers\quizController;
 
 /*
@@ -20,17 +18,6 @@ use App\Http\Controllers\quizController;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
-Route::group(['middleware' => 'auth:sanctum'], function () {
-    Route::get('me', function () {
-        return ['user'=>auth()->user()];
-    });
-    Route::get('/logout', [AuthController::class, 'logout']);
-});
-
-Route::post('/register', [AuthController::class, 'register']);
-Route::post('/login', [AuthController::class, 'login']);
-
-Route::get('/unauth', function (){return ["User"=>"Unauthenticated"];})->name('unauth');
 
 Route::post('/rating', [quizController::class, 'saveAnswers']);
 Route::get('/getResults', [quizController::class, 'getResults']);
--- routes/web.php
+++ routes/web.php
@@ -12,15 +12,10 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-use App\Http\Controllers\testController;
-use App\Http\Controllers\AuthController;
-
 
 Route::get('/', function () {
     return view('welcome');
 });
-
-Route::get('/test', [testController::class, 'index']);
 
 
 
